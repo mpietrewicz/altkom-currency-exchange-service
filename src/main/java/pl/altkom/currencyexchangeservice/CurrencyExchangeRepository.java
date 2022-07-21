186f866f4b046e7e04fffea8fed7f42c811cf0bb
mpietrewicz <mpietrewicz@gmail.com>
2022-07-21 13:19:40 +0200

    Dodanie repozytorium i deivera do bazy danych

---
+++ src/main/java/pl/altkom/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,8 @@
+package pl.altkom.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+}
